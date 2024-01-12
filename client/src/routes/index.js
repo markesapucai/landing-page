6d65efd169dd84896a4a6d462bb1d87b6c26d52c
João Vinícius <97064828+JaumDarkz@users.noreply.github.com>
2024-01-11 23:41:44 -0300

    Update index.js

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import Login from '../pages/Login';
 import Home from '../pages/Home';
@@ -7,10 +7,10 @@ import Page404 from '../pages/Page404';
 
 export default function Routees() {
   return (
-      <Routes>
+      <BrowserRouter>
         <Route path='*' element={<Page404 />}/>
         <Route path='/' element={<Home />}/>
         <Route path='/login' element={<Login />}/>
-      </Routes>
+      </BrowserRouter>
   )
 }
